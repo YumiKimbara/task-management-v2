eae357f63e82ff7975a67781797c68946a0e4f22
Yumi <a13158y@gmail.com>
2023-03-10 15:02:08 -0800

    import dotenv, set up .env file

--- server/index.js
+++ server/index.js
@@ -2,9 +2,12 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
 import taskRoutes from "./routes/tasks.js";
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
