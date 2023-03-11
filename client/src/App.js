d90608bf0ded6a399c1959ef272f90e79c6822ee
Yumi <a13158y@gmail.com>
2023-03-11 02:12:04 -0800

    change version of dependencies, change structure of Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,34 @@
 import React, { useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Header from "./Components/layout/Header";
 import Home from "./Components/Home";
 
 import { createStyles, makeStyles } from "@material-ui/core/styles";
 
+// const useStyles = makeStyles(() =>
+//   createStyles({
+//     basicStyle: {
+//       minHeight: "calc(100vh - 130px)",
+//       display: "flex",
+//       justifyContent: "top",
+//       paddingTop: "30px",
+//       paddingBottom: "30px",
+//       flexDirection: "column",
+//       backgroundColor: "#f0efeb",
+//     },
+//   })
+// );
+
 const useStyles = makeStyles(() =>
   createStyles({
-    basicStyle: {
-      minHeight: "calc(100vh - 130px)",
+    container: {
       display: "flex",
-      justifyContent: "top",
-      paddingTop: "30px",
-      paddingBottom: "30px",
+      justifyContent: "space-between",
+      marginTop: "1rem",
+      marginLeft: "5rem",
+      marginRight: "5rem",
       flexDirection: "column",
-      backgroundColor: "#f0efeb",
     },
   })
 );
@@ -25,10 +39,19 @@ const App = () => {
 
   return (
     <>
-      <Header checkKey={checkKey} setCheckKey={setCheckKey} />
-      <div className={classes.basicStyle}>
-        <Home checkKey={checkKey} />
-      </div>
+      <Router>
+        <Header checkKey={checkKey} setCheckKey={setCheckKey} />
+        <Switch>
+          <div className={classes.container}>
+            <Route path="/" exact component={Home} />
+            <Route
+              path="/Home"
+              exact
+              render={(props) => <Home checkKey={checkKey} {...props} />}
+            />
+          </div>
+        </Switch>
+      </Router>
     </>
   );
 };
